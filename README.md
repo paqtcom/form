6843c70f9df29f19cc6eca591374a33783bda530
Pieter Wigboldus <pieter.wigboldus@way2web.nl>
2017-08-11 16:41:55 +0200

    Add the form files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Way 2 Filter
+
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url]
+
+
+
+[downloads-image]: https://img.shields.io/npm/dm/way2web-form.svg
+[npm-url]: https://www.npmjs.com/package/way2web-form
+[npm-image]: https://img.shields.io/npm/v/way2web-form.svg
---
+++ js/form.js
@@ -0,0 +1,89 @@
+/**
+ * Way 2 Filter.
+ *
+ * @return {array}
+ */
+window.W2Filter = function() {
+    'use strict';
+
+    var version = '0.0.1';
+
+    var keys = {
+        's':      'button.btn-primary[type=submit]',
+        'a':      'a.btn-default',
+        'Delete': 'button.btn-danger[type=submit]'
+    };
+
+    /**
+     * Helper functions for forms.
+     */
+    function init() {
+        $(document).keydown(keyPress);
+
+        if (!Modernizr.formattribute) {
+            $('button[type=submit][form]').on('click', submit);
+            $('form input.form-control').on('keydown', submitOnEnter);
+        }
+    }
+
+    /**
+     * Trigger the form when you click on the enter button.
+     *
+     * @param {object} event
+     */
+    function submitOnEnter(event) {
+        if (event.key == 'Enter') {
+            $(this).closest('form').submit();
+        }
+    }
+
+    /**
+     * Trigger the form when you click on a button with the form attribute.
+     * The attribute form contains the id of the form.
+     *
+     * @param {object} event
+     */
+    function submit(event) {
+        var form = $('#' + $(this).attr('form'));
+
+        event.preventDefault();
+
+        if (form) {
+            submit();
+        }
+    }
+
+    /**
+     * Check if you press a key combination if there is a element to click.
+     *
+     * @param {object} event
+     */
+    function keyPress(event) {
+        var key = keys[event.key];
+
+        if (event.ctrlKey && key) {
+            event.preventDefault();
+            click(key);
+        }
+    }
+
+    /**
+     * Click on an element.
+     *
+     * @param {string} key
+     */
+    function click(key) {
+        if ($(key).length > 0) {
+            $(key)[0].click();
+        }
+    }
+
+    return {
+        init:          init,
+        submit:        submit,
+        submitOnEnter: submitOnEnter,
+        keyPress:      keyPress,
+        click:         click,
+        version:       version
+    };
+};
