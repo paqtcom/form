8062dc7c0c6f9fa811bbc08c38c2a6aa70857445
Pieter Wigboldus <pieter@way2web.nl>
2020-09-09 15:55:09 +0200

    Remove jquery and modernizr so we have less dependencies

--- src/form.js
+++ src/form.js
@@ -1,5 +1,57 @@
-import $ from 'jquery';
-import Modernizr from 'modernizr';
+/**
+   * createElement is a convenience wrapper around document.createElement. Since we
+   * use createElement all over the place, this allows for (slightly) smaller code
+   * as well as abstracting away issues with creating elements in contexts other than
+   * HTML documents (e.g. SVG documents).
+   *
+   * @returns {HTMLElement|SVGElement} An HTML or SVG element
+   */
+function createElement() {
+    const isSVG = document.documentElement.nodeName.toLowerCase() === 'svg';
+
+    if (typeof document.createElement !== 'function') {
+        // This is the case in IE7, where the type of createElement is "object".
+        // For this reason, we cannot call apply() as Object is not a Function.
+        return document.createElement(arguments[0]);
+    } else if (isSVG) {
+        return document.createElementNS.call(document, 'http://www.w3.org/2000/svg', arguments[0]);
+    } else {
+        return document.createElement.apply(document, arguments);
+    }
+}
+
+const hasFormAttribute = () => {
+    var form = createElement('form');
+    var input = createElement('input');
+    var div = createElement('div');
+    var id = 'formtest' + (new Date()).getTime();
+    var attr;
+    var bool = false;
+
+    form.id = id;
+
+    // IE6/7 confuses the form idl attribute and the form content attribute, so we use document.createAttribute
+    try {
+        input.setAttribute('form', id);
+    } catch (e) {
+        if (document.createAttribute) {
+            attr = document.createAttribute('form');
+            attr.nodeValue = id;
+            input.setAttributeNode(attr);
+        }
+    }
+
+    div.appendChild(form);
+    div.appendChild(input);
+
+    document.documentElement.appendChild(div);
+
+    bool = form.elements && form.elements.length === 1 && input.form === form;
+
+    div.parentNode.removeChild(div);
+
+    return bool;
+};
 
 /**
  * Way2Web Form helpers.
@@ -20,23 +72,34 @@ class W2Form {
         };
         this.keys = {};
         this.keyOptions = keyOptions;
+        this.lastKey = null;
 
-        this.modern = Modernizr.formattribute;
+        this.modern = hasFormAttribute();
     }
 
 
     /**
      * Helper functions for forms.
      *
+     * @param {object} element
+     *
      * @return {object}
      */
-    init() {
-        this.keys = $.extend({}, this.defaultKeys, this.keyOptions || {});
-        $(document).on('keydown', this.keyPress.bind(this));
+    init(element) {
+        this.keys = Object.assign({}, this.defaultKeys, this.keyOptions || {});
+        element.addEventListener('keydown', this.keyPress.bind(this), false);
 
         if (!this.modern) {
-            $('button[type=submit][form]').on('click', this.submit);
-            $('form input.form-control').on('keydown', this.submitOnEnter);
+            element.querySelector('button[type=submit][form]').addEventListener(
+                'click',
+                this.submit.bind(this),
+                false
+            );
+            element.querySelector('form input.form-control').addEventListener(
+                'keydown',
+                this.submitOnEnter.bind(this),
+                false
+            );
         }
 
         return this;
@@ -71,7 +134,7 @@ class W2Form {
      */
     submitOnEnter(event) {
         if (event.key == 'Enter') {
-            $(this).closest('form').submit();
+            event.target.closest('form').submit();
         }
     }
 
@@ -82,7 +145,7 @@ class W2Form {
      * @param {object} event
      */
     submit(event) {
-        let button = $(this);
+        let button = event.target;
         let buttonName = button.attr('name');
         let buttonValue = button.val();
         let formId = button.attr('form');
@@ -96,7 +159,7 @@ class W2Form {
             return;
         }
 
-        form = $('#' + formId);
+        form = document.getElementById(formId);
         if (buttonName && buttonValue) {
             form.append('<input type="hidden" name="' + buttonName + '" value="' + buttonValue + '">');
         }
@@ -114,6 +177,8 @@ class W2Form {
     keyPress(event) {
         let key = this.keys[event.key];
 
+        this.lastKey = event.key;
+
         if (event.ctrlKey && key && event.target.tagName != 'INPUT' && event.target.tagName != 'TEXTAREA') {
             event.preventDefault();
             this.click(key);
@@ -126,10 +191,14 @@ class W2Form {
      * @param {string} key
      */
     click(key) {
-        if ($(key).length > 0) {
-            $(key)[0].click();
+        if (document.querySelectorAll(key).length > 0) {
+            document.querySelector(key).click();
         }
     }
+}
+
+if (typeof window === 'object') {
+    window.W2Form = W2Form;
 }
 
 if (typeof module === 'object' && typeof module.exports === 'object') {
--- test/form.js
+++ test/form.js
@@ -7,8 +7,12 @@
      * Call function if the dom is ready.
      */
     Way2web.onDomReady = function() {
-        Way2web.form = new W2Form().init();
+        Way2web.form = new W2Form().init(document);
     };
 })(window.Way2web = window.Way2web || {});
 
-$(document).ready(window.Way2web.onDomReady);
+if (document.readyState != 'loading') {
+    window.Way2web.onDomReady();
+} else {
+    document.addEventListener('DOMContentLoaded', window.Way2web.onDomReady);
+}
